a6727d796b5a65b01b33ff0312152cc210b0d15f
Cristian Machuca <cmachuca32@gmail.com>
2022-04-28 10:09:19 +0300

    Documentation updated

--- routes/Client.js
+++ routes/Client.js
@@ -9,10 +9,53 @@ const {
 } = require("../controllers/Client");
 const {validateClient} = require("../middleware/validateClient");
 
+/**
+ * @swagger
+ * /category:
+ *   post:
+ *     summary: Create Client
+ *     tags: [Client]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Client"
+ *     responses:
+ *       200: 
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Client"
+ *       400:
+ *         description: bad request     
+ */
 router.post("/client", validateClient, createClient);
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Client:
+ *      type: object
+ *      required:
+ *        - name
+ *      properties:
+ *        name:
+ *          type: String,
+ *        email: 
+ *          type: String,
+ *        telephone:
+ *          type: String
+ *          
+ */
 router.get("/client/get-all-clients", getClients);
 router.get("/client/edit-client/:id", getOneClient);
 router.patch("/client/update-client/:id", updateClient);
 router.delete("/client/delete-client/:id", deleteClient);
 
 module.exports = router;
+
+
+
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const { readdirSync } = require("fs");
 const connectionDB = require("./database/database");
 const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsdoc = require("Swagger-jsdoc");
+const swaggerConfig = require("./documentation/swagger.config.json");
 require("dotenv").config();
 
 const app = express();
@@ -10,8 +13,13 @@ connectionDB();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: "true" }));
+const swaggerDocs = swaggerJsdoc(swaggerConfig);
+app.use(
+  "/api/docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDocs, { explorer: true })
+);
 app.use(cors());
-
 
 readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
